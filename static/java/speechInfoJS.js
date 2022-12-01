f10e9f51e45a120abdfa7abfaf444c78fec2d456
ibstnifty <118180712+ibstnifty@users.noreply.github.com>
2022-12-01 12:55:38 +0800

    Update speechInfoJS.js

--- static/java/speechInfoJS.js
+++ static/java/speechInfoJS.js
@@ -21,7 +21,7 @@ function showText (id) {
 
 
     if (id=="1"){
-        x.innerHTML='<b>(1) Dramatic Interpretation: </b>'+
+        x.innerHTML='<b>(1) Solo Interpretation: </b>'+
         'In this event, participants will have to give a <b><u>7-10 minute</u></b> performance based on a memorized <b>speech/script/scene</b>'+
         ' from any source of media. The speaker will have to <b>memorize and physically act</b> out the script.'+
         ' Performances must also include an introduction written by the student to contextualize the performance and state the title and author.<br><br>'+
@@ -38,17 +38,7 @@ function showText (id) {
         '<b><u><i>Chemistry</i></u></b>: Did the duo give their lines in a smooth manner that seemed natural? Did both members remember their lines?'+
         '<br><u><b><i>Delivery</i></b></u>: Did the speaker have to deliver the speech smoothly? Did their voice sound confident, and did they have appropriate volume?';
     }else if (id=="3"){
-        x.innerHTML = '<b>(3) Editorial Commentary</b>: In this event participants must deliver a scripted original speech that analyzes an event that has happened in the news.'+
-        ' The speech should be between <b>1:45 and 2:00 minutes</b> long.'+
-        ' The topic is chosen by the participant before the event and can be any topic that has appeared in recent news.'+
-        ' The participant can write a script and use it during the speech.'+
-        ' During the speech the participant will be seated.<br><br>'+
-        'The speech will be graded on:   <br>'+
-        '<b><u><i>Organization</i></u></b>: How well the speaker organized their speech. Did the speech have a clear structure, and did the development of the speech go smoothly?'+
-        '<br><u><b><i>Analysis</b></i></u>: How well did the speaker address the prompt? Did the speech have points that were justified and significant?'+
-        '<br><u><b><i>Delivery</b></i></u>: Did the speaker have to deliver the speech smoothly? Did their voice sound confident, and did they have appropriate volume?';
-    }else if (id=="4"){
-        x.innerHTML='<b>(4) Impromptu Speaking:</b> The impromptu speech is an extemporaneous narration on a topic'+
+        x.innerHTML='<b>(3) Impromptu Speaking:</b> The impromptu speech is an extemporaneous narration on a topic'+
         ' unknown to the contestant until immediately before his/her performance. It is a SHORT speech'+
         ' with an effective introduction, logical development, and fitting conclusion. Each contestant will'+
         ' draw 3 possible topics from the bowl. Speakers will be given 30 seconds to choose a topic, and'+
@@ -59,6 +49,16 @@ function showText (id) {
         '<b><i><u>Analysis:</u></i></b> How well did the speaker address the prompt? Did the speech have points that were justified and significant?<br>'+
         '<b><i><u>Delivery:</u></i></b> Did the speaker have to deliver the speech smoothly? Did their voice sound confident, and did they have appropriate volume?'+
         '<br>';
+    }else if (id=="4"){
+         x.innerHTML = '<b>(4) Editorial Commentary</b>: In this event participants must deliver a scripted original speech that analyzes an event that has happened in the news.'+
+        ' The speech should be between <b>1:45 and 2:00 minutes</b> long.'+
+        ' The topic is chosen by the participant before the event and can be any topic that has appeared in recent news.'+
+        ' The participant can write a script and use it during the speech.'+
+        ' During the speech the participant will be seated.<br><br>'+
+        'The speech will be graded on:   <br>'+
+        '<b><u><i>Organization</i></u></b>: How well the speaker organized their speech. Did the speech have a clear structure, and did the development of the speech go smoothly?'+
+        '<br><u><b><i>Analysis</b></i></u>: How well did the speaker address the prompt? Did the speech have points that were justified and significant?'+
+        '<br><u><b><i>Delivery</b></i></u>: Did the speaker have to deliver the speech smoothly? Did their voice sound confident, and did they have appropriate volume?';
     }else{
         x.innerHTML='<b>(5) Story-Telling: </b>'+
         'In this event participants will have to tell a story as if they are presenting it to an audience.'+
