312f7613b58ef2558bc8ad53c2c2694343e6f190
ibstnifty <118180712+ibstnifty@users.noreply.github.com>
2023-03-12 23:31:13 +0800

    Update awardJS.js

--- static/java/awardJS.js
+++ static/java/awardJS.js
@@ -45,92 +45,17 @@ function updateNames(){
 }
 
 //Values for each policy grid
-var ms1 = {"Anca Li & Jazlyn Shen":"Anca Li", "Wei Teng (Austin) Wang & Maggie Yumeng Lee":"Wei Teng (Austin) Wang", "Karim Erraji & William Tzu-Yang Jen":"Karim Erraji",
-           "Charlotte Wang & Catherine Wu":"Charlotte Wang", "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":"Charlotte Anthea Lloyd", "Oliver (Hsun) OuYang & Ian Milo Cheng":"Oliver (Hsun) OuYang",
-           "Wen Ling Catherine, Lee & Sabrina Chu":"Wen Ling Catherine, Lee", "Laila Erraji & Chelsea TsaiHsi Chiang":"Laila Erraji", "Austin Lee & Dylan Pak":"Austin Lee",
-           "Shannon Lo & Shaun Lee":"Shannon Lo", "Benjamin George Smith & Barkley William Wilfred Wang":"Benjamin George Smith", "Eliana Ko & Nethika Jayith Rajapakse":"Eliana Ko"
-          };
+// var ms1 = {"Anca Li & Jazlyn Shen":"Anca Li", "Wei Teng (Austin) Wang & Maggie Yumeng Lee":"Wei Teng (Austin) Wang", "Karim Erraji & William Tzu-Yang Jen":"Karim Erraji",
+//            "Charlotte Wang & Catherine Wu":"Charlotte Wang", "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":"Charlotte Anthea Lloyd", "Oliver (Hsun) OuYang & Ian Milo Cheng":"Oliver (Hsun) OuYang",
+//            "Wen Ling Catherine, Lee & Sabrina Chu":"Wen Ling Catherine, Lee", "Laila Erraji & Chelsea TsaiHsi Chiang":"Laila Erraji", "Austin Lee & Dylan Pak":"Austin Lee",
+//            "Shannon Lo & Shaun Lee":"Shannon Lo", "Benjamin George Smith & Barkley William Wilfred Wang":"Benjamin George Smith", "Eliana Ko & Nethika Jayith Rajapakse":"Eliana Ko"
+//           };
 
-var ms2 = {"Anca Li & Jazlyn Shen":"Jazlyn Shen", "Wei Teng (Austin) Wang & Maggie Yumeng Lee":"Maggie Yumeng Lee", "Karim Erraji & William Tzu-Yang Jen":"William Tzu-Yang Jen",
-           "Charlotte Wang & Catherine Wu":"Catherine Wu", "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":"Stanley Yi-Hao Jen", "Oliver (Hsun) OuYang & Ian Milo Cheng":"Ian Milo Cheng",
-           "Wen Ling Catherine, Lee & Sabrina Chu":"Sabrina Chu", "Laila Erraji & Chelsea TsaiHsi Chiang":"Chelsea TsaiHsi Chiang", "Austin Lee & Dylan Pak":"Dylan Pak",
-           "Shannon Lo & Shaun Lee":"Shaun Lee", "Benjamin George Smith & Barkley William Wilfred Wang":"Barkley William Wilfred Wang", "Eliana Ko & Nethika Jayith Rajapakse":"Nethika Jayith Rajapakse"
-          };
-
-var r1M1Score = {
-  "Anca Li & Jazlyn Shen":30,
-  "Wei Teng (Austin) Wang & Maggie Yumeng Lee":17,
-  "Karim Erraji & William Tzu-Yang Jen":24,
-  "Charlotte Wang & Catherine Wu":21,
-  "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":23,
-  "Oliver (Hsun) OuYang & Ian Milo Cheng":21,
-  "Wen Ling Catherine, Lee & Sabrina Chu":22,
-  "Laila Erraji & Chelsea TsaiHsi Chiang":24,
-  "Austin Lee & Dylan Pak":25,
-  "Shannon Lo & Shaun Lee":17,
-  "Benjamin George Smith & Barkley William Wilfred Wang":22,
-  "Eliana Ko & Nethika Jayith Rajapakse":30
-};
-
-var r1M2Score = {
-  "Anca Li & Jazlyn Shen":30,
-  "Wei Teng (Austin) Wang & Maggie Yumeng Lee":17,
-  "Karim Erraji & William Tzu-Yang Jen":24,
-  "Charlotte Wang & Catherine Wu":18,
-  "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":11,
-  "Oliver (Hsun) OuYang & Ian Milo Cheng":11,
-  "Wen Ling Catherine, Lee & Sabrina Chu"23,
-  "Laila Erraji & Chelsea TsaiHsi Chiang":24,
-  "Austin Lee & Dylan Pak":20,
-  "Shannon Lo & Shaun Lee":21,
-  "Benjamin George Smith & Barkley William Wilfred Wang":26,
-  "Eliana Ko & Nethika Jayith Rajapakse":26
-};
-
-var r1TScore = {
-  "Anca Li & Jazlyn Shen":60,
-  "Wei Teng (Austin) Wang & Maggie Yumeng Lee":34,
-  "Karim Erraji & William Tzu-Yang Jen":48,
-  "Charlotte Wang & Catherine Wu":39,
-  "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":34,
-  "Oliver (Hsun) OuYang & Ian Milo Cheng":32,
-  "Wen Ling Catherine, Lee & Sabrina Chu":45,
-  "Laila Erraji & Chelsea TsaiHsi Chiang":48,
-  "Austin Lee & Dylan Pak":45,
-  "Shannon Lo & Shaun Lee":38,
-  "Benjamin George Smith & Barkley William Wilfred Wang":48
-  "Eliana Ko & Nethika Jayith Rajapakse":56
-};
-
-var r1OpScore = {
-  "Anca Li & Jazlyn Shen":60,
-  "Wei Teng (Austin) Wang & Maggie Yumeng Lee":34,
-  "Karim Erraji & William Tzu-Yang Jen":48,
-  "Charlotte Wang & Catherine Wu":39,
-  "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":34,
-  "Oliver (Hsun) OuYang & Ian Milo Cheng":32,
-  "Wen Ling Catherine, Lee & Sabrina Chu":45,
-  "Laila Erraji & Chelsea TsaiHsi Chiang":48,
-  "Austin Lee & Dylan Pak":45,
-  "Shannon Lo & Shaun Lee":38,
-  "Benjamin George Smith & Barkley William Wilfred Wang":48
-  "Eliana Ko & Nethika Jayith Rajapakse":56
-};
-
-var Result1 = {
-  "Anca Li & Jazlyn Shen":60,
-  "Wei Teng (Austin) Wang & Maggie Yumeng Lee":34,
-  "Karim Erraji & William Tzu-Yang Jen":48,
-  "Charlotte Wang & Catherine Wu":39,
-  "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":34,
-  "Oliver (Hsun) OuYang & Ian Milo Cheng":32,
-  "Wen Ling Catherine, Lee & Sabrina Chu":45,
-  "Laila Erraji & Chelsea TsaiHsi Chiang":48,
-  "Austin Lee & Dylan Pak":45,
-  "Shannon Lo & Shaun Lee":38,
-  "Benjamin George Smith & Barkley William Wilfred Wang":48
-  "Eliana Ko & Nethika Jayith Rajapakse":56
-};
+// var ms2 = {"Anca Li & Jazlyn Shen":"Jazlyn Shen", "Wei Teng (Austin) Wang & Maggie Yumeng Lee":"Maggie Yumeng Lee", "Karim Erraji & William Tzu-Yang Jen":"William Tzu-Yang Jen",
+//            "Charlotte Wang & Catherine Wu":"Catherine Wu", "Charlotte Anthea Lloyd & Stanley Yi-Hao Jen":"Stanley Yi-Hao Jen", "Oliver (Hsun) OuYang & Ian Milo Cheng":"Ian Milo Cheng",
+//            "Wen Ling Catherine, Lee & Sabrina Chu":"Sabrina Chu", "Laila Erraji & Chelsea TsaiHsi Chiang":"Chelsea TsaiHsi Chiang", "Austin Lee & Dylan Pak":"Dylan Pak",
+//            "Shannon Lo & Shaun Lee":"Shaun Lee", "Benjamin George Smith & Barkley William Wilfred Wang":"Barkley William Wilfred Wang", "Eliana Ko & Nethika Jayith Rajapakse":"Nethika Jayith Rajapakse"
+//           };
 
 var scoreBoard = {
   "Anca Li & Jazlyn Shen":"static/images/policy1.png",
